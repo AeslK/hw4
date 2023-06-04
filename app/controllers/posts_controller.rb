7b5391820dec74717b04520786b77bdc1723807f
Aesl Kim <aesl.kim@kellogg.northwestern.edu>
2023-06-04 07:46:25 +0000

    third commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,14 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
-    @post["title"] = params["post"]["title"]
-    @post["description"] = params["post"]["description"]
-    @post["posted_on"] = params["post"]["posted_on"]
-    @post["place_id"] = params["post"]["place_id"]
-    @post["user_id"] = @current_user["id"]
-    @post.save
+      @post = Post.new
+      @post["title"] = params["post"]["title"]
+      @post["description"] = params["post"]["description"]
+      @post.uploaded_image.attach(params["post"]["uploaded_image"])
+      @post["posted_on"] = params["post"]["posted_on"]
+      @post["place_id"] = params["post"]["place_id"]
+      @post["user_id"] = @current_user["id"]
+      @post.save
     redirect_to "/places/#{@post["place_id"]}"
   end
 
